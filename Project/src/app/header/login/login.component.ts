8a358e60ac9802fee33c9d974510c63ddb8ca820
andresm96 <andres-malvestiti@hotmail.com>
2017-09-27 16:05:29 -0300

    experimentando con login

--- Project/src/app/header/login/login.component.ts
+++ Project/src/app/header/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Services } from '../../../app/services';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [Services]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private services: Services) { }
 
   ngOnInit() {
   }
 
+  signIn(user: string, password: string) {
+    
+          this.services.authentication(user, password);
+  }
 }
